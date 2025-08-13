5c2e61b323cfdedb1845a4f726a9ded9d1666cf7
João G. Bortoletto <jgb3532@hotmail.com>
2025-08-13 07:57:16 -0300

    feat: initialize memory and create show memory function

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MEM_SIZE 32 
+
+void show_mem(unsigned char *mem) {
+    printf("Memory:\n");
+    for (int i = 0; i < MEM_SIZE; i++) {
+        printf("[%02d] %3d\t", i, mem[i]);
+        if ((i + 1) % 8 == 0) printf("\n");
+    }
+}
+
 int main() {
     return 0;
 }
