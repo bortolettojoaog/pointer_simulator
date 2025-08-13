1dfc9e11e164cc235c38b3d87cd31ed938cdfd87
João G. Bortoletto <jgb3532@hotmail.com>
2025-08-13 08:00:05 -0300

    feat: create a interactive menu

--- main.c
+++ main.c
@@ -15,5 +15,28 @@ int main() {
     unsigned char mem[MEM_SIZE] = {0};
     unsigned char *pointer = mem;
 
+    int option;
+    do {
+        show_mem(mem);
+        printf("\nPointer is located at %ld (value = %d)\n", pointer - mem, *pointer);
+        printf("1 - Go Next\n");
+        printf("2 - Go Back\n");
+        printf("3 - Change Value\n");
+        printf("4 - Exit\n");
+        printf("Option: ");
+        scanf("%d", &option);
+
+        if (option == 1 && (pointer - mem) < MEM_SIZE - 1) {
+            pointer++;
+        } else if (option == 2 && (pointer - mem) > 0) {
+            pointer--;
+        } else if (option == 3) {
+            int val;
+            printf("New Value: ");
+            scanf("%d", &val);
+            *pointer = (unsigned char) val;
+        }
+    } while (option != 4);
+
     return 0;
 }
